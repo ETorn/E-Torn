4328eb1db81611159060cef72dcd0935abefdd99
Guillem Cruz <gcquiroga@gmail.com>
2017-03-11 20:28:28 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 156021e88e006d50da0f21078747bd7baff68200, reversing
    changes made to 4c02979cdaa8fae12712a4b7f8c64827e84b4f24.

--- app/src/main/java/com/example/admin/e_torn/UserTurnInfo.java
+++ app/src/main/java/com/example/admin/e_torn/UserTurnInfo.java
@@ -2,41 +2,12 @@ package com.example.admin.e_torn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.example.admin.e_torn.Listeners.PushUpdateListener;
-import com.google.firebase.messaging.RemoteMessage;
 
 public class UserTurnInfo extends AppCompatActivity {
-
-    AppCompatActivity self;
-    TopicSubscription storeSubscription;
-    String storeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_turn_info);
-
-        storeId = getIntent().getStringExtra("id");
-        storeSubscription = new TopicSubscription(this, "store." + storeId);
-        storeSubscription.setListener(new PushUpdateListener() {
-            @Override
-            public void onPushUpdate(RemoteMessage remoteMessage) {
-                Toast.makeText(self, "PUSH", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        storeSubscription.subscribe();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        storeSubscription.unsubscribe();
     }
 }
