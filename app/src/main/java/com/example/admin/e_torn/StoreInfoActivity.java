055da579858a739c2cf25f18a46dd6d4041cce8e
VictorPuig <vicpuigcer@gmail.com>
2017-05-23 20:14:38 +0200

    Fixed temps aprox a StoreInfoActivity

--- app/src/main/java/com/example/admin/e_torn/StoreInfoActivity.java
+++ app/src/main/java/com/example/admin/e_torn/StoreInfoActivity.java
@@ -151,8 +151,8 @@ public class StoreInfoActivity extends BaseActivity implements View.OnClickListe
                 }
 
                 if (remoteMessage.getData().get("aproxTime") != null) {
-                    store.setAproxTime(Math.round(Float.parseFloat(remoteMessage.getData().get("aproxTime"))));
-                    Log.d(TAG, "User aproxTime received: " + store.getAproxTime());
+                   app.getUserInfo().get(store.get_id()).setAproxTime(Math.round(Float.parseFloat(remoteMessage.getData().get("aproxTime"))));
+                    Log.d(TAG, "User aproxTime received: " + Float.parseFloat(remoteMessage.getData().get("aproxTime")));
                 }
                 updateUI();
             }
@@ -327,16 +327,54 @@ public class StoreInfoActivity extends BaseActivity implements View.OnClickListe
         }
         //queueText.setText(String.valueOf(store.getReloadedQueue()) + " torns");
 
-        if ((store.getAproxTime() == 0 && !inTurn()) || (inTurn() && app.getUserInfo().get(store.get_id()).getAproxTime() == 0)) {
+
+        if (!inTurn()) {
+            if (Math.round(store.getAproxTime()) < 1)
+               setTimeLabelsVisibility(false);
+
+            else {
+                setTimeLabelsVisibility(true);
+                float aproxTimeAux = store.getAproxTime();
+                aproxTime.setText(String.valueOf(Math.round(aproxTimeAux)) + " " + getString(R.string.minutes)); // arrodonim al int mes proper al numero decimal que rebem del servidor
+            }
+        }
+
+        else {
+            if (Math.round(app.getUserInfo().get(store.get_id()).getAproxTime()) < 1)
+                setTimeLabelsVisibility(false);
+            else {
+                setTimeLabelsVisibility(true);
+                float aproxTimeAux = app.getUserInfo().get(store.get_id()).getAproxTime();
+                aproxTime.setText(String.valueOf(Math.round(aproxTimeAux)) + " " + getString(R.string.minutes)); // arrodonim al int mes proper al numero decimal que rebem del servidor
+            }
+        }
+
+        /*if ((store.getAproxTime() == 0 && !inTurn()) || (inTurn() && Math.round(app.getUserInfo().get(store.get_id()).getAproxTime()) < 1)) {
             timeIcon.setVisibility(View.GONE);
             aproxTime.setVisibility(View.GONE);
+            Log.d(TAG, "StoreAproxTime: " + String.valueOf(Math.round(store.getAproxTime())));
+            Log.d(TAG, "UserAproxTimeRound: " + String.valueOf(Math.round(app.getUserInfo().get(store.get_id()).getAproxTime())));
         }
 
         else {
             timeIcon.setVisibility(View.VISIBLE);
             aproxTime.setVisibility(View.VISIBLE);
             float aproxTimeAux = inTurn() ? app.getUserInfo().get(store.get_id()).getAproxTime() : store.getAproxTime();
+            Log.d(TAG, "StoreAproxTime: " + String.valueOf(Math.round(store.getAproxTime())));
+            Log.d(TAG, "UserAproxTimeRound: " + String.valueOf(Math.round(app.getUserInfo().get(store.get_id()).getAproxTime())));
+            Log.d(TAG, "UserAproxTime: " + String.valueOf(app.getUserInfo().get(store.get_id()).getAproxTime()));
             aproxTime.setText(String.valueOf(Math.round(aproxTimeAux)) + " " + getString(R.string.minutes)); // arrodonim al int mes proper al numero decimal que rebem del servidor
+        }*/
+    }
+
+    public void setTimeLabelsVisibility (boolean state) {
+        if (state) {
+            timeIcon.setVisibility(View.VISIBLE);
+            aproxTime.setVisibility(View.VISIBLE);
+        }
+        else {
+            timeIcon.setVisibility(View.GONE);
+            aproxTime.setVisibility(View.GONE);
         }
     }
 
