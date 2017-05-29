fad185d0c49c0bc74c07dd85b9aabb833ed576dc
Guillem Cruz <gcquiroga@gmail.com>
2017-05-29 12:58:55 +0200

    Docs constants

--- app/src/main/java/com/example/admin/e_torn/Constants.java
+++ app/src/main/java/com/example/admin/e_torn/Constants.java
@@ -1,27 +1,44 @@
 package com.example.admin.e_torn;
 
-/**
- * Created by Admin on 06/02/2017.
- */
 
 public class Constants {
 
-    /** Duration of wait **/
-    /** Called when the activity is first created. */
+    /**
+     * Duració de la splash screen
+     */
     static final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    /**
+     * Etiqueta de failure de retrofit
+     */
     static final String RETROFIT_FAILURE_TAG = "RETROFIT_FAILURE";
 
     public static final String serverURL = "http://192.168.1.128:8080";
 
     public static final String caesarURL = "http://192.168.1.128:8081";
 
+    /**
+     * Nom del paquet. Ha de concordar amb el de veritat
+     */
     public static final String packageName = "com.example.admin.e_torn";
 
+    /**
+     * Etiqueta sota la que es guarden les perferencies de la app
+     */
     public static final String PREFERENCES_NAME = packageName + "UserPreferences";
 
+    /**
+     * Nom de la propietat de preferencies on es guarda el token de fce
+     */
     public static final String FCE_TOKEN_NAME = "FCEToken";
 
+    /**
+     * Nom de la propietat de preferencies on es guarda la id de user
+     */
     public static final String USER_ID = "userId";
 
-    public static int DEFAULT_DISTANCE = 1000;
+    /**
+     * Distància maxima en la que es buscaràn supers
+     */
+    public static int DEFAULT_DISTANCE = 3000;
 }
